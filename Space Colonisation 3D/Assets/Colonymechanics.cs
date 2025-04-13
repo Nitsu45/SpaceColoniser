b1170660be0214abaa1ca1309cf3d55045fd5433
Nitsu45 <nicobalke@gmx.de>
2025-04-13 22:38:27 +0200

    Implementation of provisional UI

--- Space Colonisation 3D/Assets/Colonymechanics.cs
+++ Space Colonisation 3D/Assets/Colonymechanics.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Colonymechanics : MonoBehaviour
 {
+    //Reference
+    public Text OreProductionDisplay;
+    public Text OreDisplay;
+    public Text EnergyProductionDisplay;
+    public Text ManPowerDisplay;
+
+
+
+    //Global variables
     public int oreProduction = 0;
     public int energyProduction = 0;
     public int manpower = 0;
 
 
-    int[] planetStorage = new int[2] {0,0};
+    int[] planetStorage = new int[3] {0,0,0};
     List<string> colonyBuildingsList = new List<string>();
 
     // Start is called before the first frame update
@@ -40,8 +51,12 @@ public class Colonymechanics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        planetStorage[0] =+ oreProduction; 
-        planetStorage[1] =+ energyProduction;
+        planetStorage[0] = planetStorage[0] + oreProduction; 
+        planetStorage[1] = energyProduction;
         planetStorage[2] = manpower;
+        OreProductionDisplay.text = $"Ore Production: {oreProduction}";
+        OreDisplay.text = $"Ore: {planetStorage[0]}";
+        EnergyProductionDisplay.text = $"Energy Production: {planetStorage[1]}";
+        ManPowerDisplay.text = $"Man Power:  {planetStorage[2]}";
     }
 }
