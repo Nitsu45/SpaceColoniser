68eea9c28cf4dbc8f3e826554c4912eadafb1ba4
Nitsu45 <nicobalke@gmx.de>
2025-04-14 18:37:14 +0200

    Implementing core Gameplay mechanics
    Implementing constructing buildings

--- Space Colonisation 3D/Assets/Colonymechanics.cs
+++ Space Colonisation 3D/Assets/Colonymechanics.cs
@@ -18,6 +18,7 @@ public class Colonymechanics : MonoBehaviour
     public int oreProduction = 0;
     public int energyProduction = 0;
     public int manpower = 0;
+    public float TickTimer = 60f;
 
 
     int[] planetStorage = new int[3] {0,0,0};
@@ -26,27 +27,8 @@ public class Colonymechanics : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Check buildings list and adjust resource production
-        foreach (var item in colonyBuildingsList)
-        {
-            switch(item)
-            {
-                
-                case "mine":
-                    oreProduction =+ 50;
-                    break;
-                case "energyProduction":
-                    energyProduction =+ 50;
-                    break;
-                case "house":
-                    manpower =+ 50;
-                    break;
-                default:
-                    Debug.Log("No resource Building");
-                    break;
-            }
-        }
-
+        //Checking already existing Buildings
+        checkBuildingsList();
         //the routine to update the resources
         StartCoroutine(ResourceUpdate());
     }
@@ -54,10 +36,49 @@ public class Colonymechanics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UpdateDisplay();
+    }
+
+    void checkBuildingsList()
+    {
+        int[] temporaryProductionCounter = new int[3];
         
+        //Check buildings list and adjust resource production
+        foreach (var item in colonyBuildingsList)
+        {
+            switch (item)
+            {
+
+                case "mine":
+                    temporaryProductionCounter[0] = temporaryProductionCounter[0] + 50;
+                    break;
+                case "powerplant":
+                    temporaryProductionCounter[1] = temporaryProductionCounter[1] + 50;
+                    break;
+                case "house":
+                    temporaryProductionCounter[2] = temporaryProductionCounter[2] + 50;
+                    break;
+                default:
+                    Debug.Log("No resource Building");
+                    break;
+            }
+        }
+        oreProduction = temporaryProductionCounter[0];
+        energyProduction = temporaryProductionCounter[1];
+        manpower = temporaryProductionCounter[2];
+    }
+
+    void UpdateDisplay()
+    {
+        OreProductionDisplay.text = $"Ore Production: {oreProduction}/m";
+        OreDisplay.text = $"Ore: {planetStorage[0]}";
+        EnergyProductionDisplay.text = $"Energy Production: {planetStorage[1]}";
+        ManPowerDisplay.text = $"Man Power:  {planetStorage[2]}";
     }
 
 
+
+    //Resource update that adds the resources every 10 seconds
     IEnumerator ResourceUpdate()
     {
         while(true)
@@ -65,13 +86,50 @@ public class Colonymechanics : MonoBehaviour
             planetStorage[0] = planetStorage[0] + oreProduction;
             planetStorage[1] = energyProduction;
             planetStorage[2] = manpower;
-            OreProductionDisplay.text = $"Ore Production: {oreProduction}/m";
-            OreDisplay.text = $"Ore: {planetStorage[0]}";
-            EnergyProductionDisplay.text = $"Energy Production: {planetStorage[1]}";
-            ManPowerDisplay.text = $"Man Power:  {planetStorage[2]}";
-            yield return new WaitForSecondsRealtime(60f);
+            yield return new WaitForSecondsRealtime(TickTimer);
         }
     }
+    //Button functions
 
+    public void BuyMineButton()
+    {
+        if (planetStorage[0] >= 200)
+        {
+            colonyBuildingsList.Add("mine");
+            planetStorage[0] = planetStorage[0] - 200;
+            checkBuildingsList();
+        }
+        else
+        {
+            Debug.Log("Not enough Ore!");
+        }
+     
+    }
+    public void BuyPPButton()
+    {
+        if (planetStorage[0] >= 200)
+        {
+            colonyBuildingsList.Add("powerplant");
+            planetStorage[0] = planetStorage[0] - 200;
+            checkBuildingsList();
+        }
+        else
+        {
+            Debug.Log("Not enough Ore!");
+        }
+    }
+    public void BuyHouseButton()
+    {
+        if (planetStorage[0] >= 200)
+        {
+            colonyBuildingsList.Add("house");
+            planetStorage[0] = planetStorage[0] - 200;
+            checkBuildingsList();
+        }
+        else
+        {
+            Debug.Log("Not enough Ore!");
+        }
+    }
 
 }
