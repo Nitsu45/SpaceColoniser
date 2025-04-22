a2f8516f77e65c67296d956f2b4abed1d9faf817
Nitsu45 <nicobalke@gmx.de>
2025-04-22 15:41:53 +0200

    Complete Overhaul of the Constructionmechanics
    Complete Overhaul of the construction mechanics and the UI mechanic

--- Space Colonisation 3D/Assets/Scripts/UIScript.cs
+++ Space Colonisation 3D/Assets/Scripts/UIScript.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
+    //References
+    public Camera mainCamera;
+    public Text OreProductionDisplay;
+    public Text OreDisplay;
+    public Text EnergyProductionDisplay;
+    public Text ManPowerDisplay;
+
+    public GameObject SelectedPlanet;
+
+    Colonymechanics SelectedColony;
+    ConstructionMechanics ConstructionScript;
+
+    //Global variables
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SelectedColony = SelectedPlanet.GetComponent<Colonymechanics>();
+        ConstructionScript = GetComponent<ConstructionMechanics>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateDisplay();
+        lookAroundPlanet();
     }
+
+    //Function to look around the Planet with the mouse
+    void lookAroundPlanet()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * 20;
+
+            mainCamera.transform.RotateAround(transform.position, new Vector3(0, mouseX, 0), 200f * Time.deltaTime);
+        }
+    }
+
+    //Updates the displayed text with the current values of the selected Colony
+    void UpdateDisplay()
+    {
+        OreProductionDisplay.text = $"Ore Production: {SelectedColony.oreProduction}/m";
+        OreDisplay.text = $"Ore: {SelectedColony.planetStorage[0]}";
+        EnergyProductionDisplay.text = $"Energy Production: {SelectedColony.planetStorage[1]}";
+        ManPowerDisplay.text = $"Man Power:  {SelectedColony.planetStorage[2]}";
+    }
+
+    //Button functions
+    //Button that allows you to buy a mine (temporary feature)
+    public void BuyMineButton()
+    {
+        //The 
+        if (ConstructionScript.ConstructingBuilding("mine", SelectedColony.planetStorage))
+        {
+            SelectedColony.AddingBuildingToColony("mine");
+            SelectedColony.planetStorage = ConstructionScript.SubstractingConstrctuionCosts("mine", SelectedColony.planetStorage);
+        }
+        else Debug.Log("Building could not be constructed");
+    }
+    //Button that allows you to buy a powerplant (temporary feature)
+    public void BuyPPButton()
+    {
+        //Inactive
+    }
+    //Button that allows you to buy a house (temporary feature)
+    public void BuyHouseButton()
+    {
+        //Inactive
+    }
+
 }
